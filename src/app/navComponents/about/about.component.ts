74ada93d2f108a19fa87799ab4e16e0ff46bba36
shadab baig <ashadab124@gmail.com>
2023-04-29 00:04:34 +0500

    mobile view about component complete

--- src/app/navComponents/about/about.component.ts
+++ src/app/navComponents/about/about.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   public show = true;
+  window: any = window;
   public currentView: 'desktop' | 'tablet' | 'mobile' = 'desktop';
 
   constructor() { }
